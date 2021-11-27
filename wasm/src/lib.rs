d71ed2c2955d279cebed0eaf7942403ab2797848
Mátyás Fodor <matyas.fodor@gmail.com>
2021-11-27 23:06:30 +0100

    feat: add rust binding

---
+++ wasm/src/lib.rs
@@ -0,0 +1,25 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn big_computation() {
+    alert("Big computation in Rust");
+}
+
+#[wasm_bindgen]
+pub fn welcome(name: &str) {
+    alert(&format!("Hello {}, from Rust!", name));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
