ec6fcb7be51cb70b2d4c6972192103016fb1968d
Mátyás Fodor <matyas.fodor@gmail.com>
2021-12-04 17:25:26 +0100

    feat: d04 second

--- wasm/src/day04.rs
+++ wasm/src/day04.rs
@@ -8,7 +8,8 @@ struct TableCounter {
   column_count: [usize; 5],
 }
 
-fn solution1(guessed_numbers: &Vec<usize>, tables: &Vec<Vec<Vec<usize>>>) -> usize {
+fn solution(guessed_numbers: &Vec<usize>, tables: &Vec<Vec<Vec<usize>>>, second: bool) -> usize {
+  let mut tables_playing: HashSet<usize> = (1..tables.len()).collect();
   let mut elements_visited = HashSet::<usize>::new();
   let mut table_counters: Vec<TableCounter> = vec![
     TableCounter {
@@ -50,21 +51,29 @@ fn solution1(guessed_numbers: &Vec<usize>, tables: &Vec<Vec<Vec<usize>>>) -> usi
           .expect("Table not found");
         (*table).column_count[*column_index] += 1;
         (*table).row_count[*row_index] += 1;
+
         if (*table).column_count[*column_index] == 5 || (*table).row_count[*row_index] == 5 {
-          let sum_non_marked: usize = tables[*table_index]
-            .iter()
-            .flat_map(|row| row.iter().filter(|e| !elements_visited.contains(e)))
-            .sum();
-          return number * sum_non_marked;
+          if !second {
+            let sum_non_marked: usize = tables[*table_index]
+              .iter()
+              .flat_map(|row| row.iter().filter(|e| !elements_visited.contains(e)))
+              .sum();
+            return number * sum_non_marked;
+          } else {
+            tables_playing.remove(table_index);
+            if tables_playing.len() == 0 {
+              let sum_non_marked: usize = tables[*table_index]
+                .iter()
+                .flat_map(|row| row.iter().filter(|e| !elements_visited.contains(e)))
+                .sum();
+              return number * sum_non_marked;
+            }
+          }
         }
       }
     }
   }
   panic!("Couldn't find bingo");
-}
-
-fn solution2() -> usize {
-  0
 }
 
 pub fn main(s: &str, second: bool) -> usize {
@@ -104,11 +113,7 @@ pub fn main(s: &str, second: bool) -> usize {
     })
     .collect();
 
-  if second {
-    solution2()
-  } else {
-    solution1(&guessed_numbers, &tables)
-  }
+  solution(&guessed_numbers, &tables, second)
 }
 
 #[cfg(test)]
@@ -124,6 +129,6 @@ mod tests {
   fn d04_second() {
     let input = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\n22 13 17 11  0\n 8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n\n 3 15  0  2 22\n 9 18 13 17  5\n19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7\n";
     let res = super::main(input, true);
-    assert_eq!(res, 5);
+    assert_eq!(res, 1924);
   }
 }
