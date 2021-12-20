965a8e9ab84f407c147c9db23521335115df01fa
Mátyás Fodor <matyas.fodor@gmail.com>
2021-12-20 07:38:49 +0100

    feat: som progress with day 17

--- wasm/src/day17.rs
+++ wasm/src/day17.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use regex::Regex;
 use std::vec::Vec;
 
-fn is_within(
+fn is_within_inner(
   x: isize,
   y: isize,
   x_speed: isize,
@@ -15,21 +15,26 @@ fn is_within(
   if x1 <= x && x <= x2 && y1 <= y && y <= y2 {
     true
   } else {
-    // Getting farther away in both directions - return false
-    // let's check y too
-    let x_approaching = x_speed != 0 && (x1 - x) / x_speed > 0;
-    let y_approaching = y_speed != 0 && (y1 - y) / y_speed > 0;
-    if !x_approaching && !y_approaching {
+    println!(
+      "x: {}, y: {}, x speed: {}, y speed: {}",
+      x, y, x_speed, y_speed
+    );
+
+    // let x_approaching = x_speed != 0 && (x1 - x) / x_speed > 0;
+    // let y_approaching = y_speed != 0 && (y1 - y) / y_speed > 0;
+
+    // if !x_approaching && !y_approaching {
+    if x2 < x || y2 < y {
       false
     } else {
-      let new_x_speed = if x < 0 {
-        x + 1
-      } else if x > 0 {
-        x - 1
+      let new_x_speed = if x_speed < 0 {
+        x_speed + 1
+      } else if x_speed > 0 {
+        x_speed - 1
       } else {
         0
       };
-      is_within(
+      is_within_inner(
         x + x_speed,
         y + y_speed,
         new_x_speed,
@@ -43,8 +48,36 @@ fn is_within(
   }
 }
 
+fn is_within(x_speed: isize, y_speed: isize, x1: isize, x2: isize, y1: isize, y2: isize) -> bool {
+  println!(
+    "Is within called with {} {} x={}..{} y={}..{}",
+    x_speed, y_speed, x1, x2, y1, y2
+  );
+  is_within_inner(0, 0, x_speed, y_speed, x1, x2, y1, y2)
+}
+
+fn max_y(original_y: isize) -> isize {
+  (original_y * (original_y + 1)) / 2
+}
+
 fn solution1(x1: isize, x2: isize, y1: isize, y2: isize) -> isize {
-  0
+  // for i in 0..20 {
+  //   for y in 0..20 {
+
+  //   }
+  // }
+  let passing_coords: Vec<(isize, isize)> = (0..20)
+    .cartesian_product(0..20)
+    .filter(|(x, y)| is_within(*x, *y, x1, x2, y1, y2))
+    .collect();
+
+  println!("Vector {:#?}", passing_coords);
+
+  passing_coords
+    .iter()
+    .map(|(_, y)| max_y(*y))
+    .max()
+    .expect("expected to have a max")
 }
 
 pub fn main(s: &str, second: bool) -> isize {
@@ -67,7 +100,13 @@ mod tests {
   fn day17_first() {
     let input = "target area: x=20..30, y=-10..-5";
     let res = super::main(input, false);
-    assert_eq!(res, 7);
+    assert_eq!(res, 45);
+  }
+
+  #[test]
+  fn day17_is_within() {
+    let res = super::is_within(6, 9, 20, 30, -10, -5);
+    assert_eq!(res, true);
   }
 
   #[test]
